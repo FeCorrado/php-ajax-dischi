1f1a562dc5a279e8e4d9b92600b45adfca82847b
Corrado.Ferra <corradoferra@gmail.com>
2022-02-17 18:47:42 +0100

    codice da rivedere (non riesco a capire cosa non mi fa stampare)

--- index2.php
+++ index2.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.26.0/axios.min.js" integrity="sha512-bPh3uwgU5qEMipS/VOmRqynnMXGGSRv+72H/N260MQeXZIK4PG48401Bsby9Nq5P5fz7hy5UGNmC/W1Z51h2GQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <title>Document</title>
 </head>
 
@@ -16,7 +17,16 @@
         <img src="img/Spotify_Logo_CMYK_Green.png" alt="">
     </div>
     <div id="app">
-
+        <div class="big-box">
+            <div class="disc-container">
+                <div class="card" v-for="(disco, index) in dischi" :key="index">
+                    <img :src="disco.poster" alt="">
+                    <h3>result.data[index].title</h3> <!-- prove per capire se sbaglio qulacosa nella scrittura-->
+                    <h5>{{result.data[0].title}}</h5> <!-- prove per capire se sbaglio qulacosa nella scrittura-->
+                    <h5>{{disco.year}}</h5>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 
@@ -33,7 +43,7 @@
                 .get("http://localhost:8888/esercizi/php-ajax-dischi/api/dischi.php")
                 .then((result) => {
                     this.dichi = result.data;
-                    console.log(result)
+                    console.log(result.data[0].title)
                 });
         },
 
